dc47185a3863511d15fd1cae1268e12f60f27d70
admgk <adamgorz@gmail.com>
2024-07-25 22:05:09 +0200

    RollHistory is generated based on saved results /in progress/

--- app/src/main/java/pl/ignismark/luckydice/RollHistory.kt
+++ app/src/main/java/pl/ignismark/luckydice/RollHistory.kt
@@ -1,12 +1,7 @@
 package pl.ignismark.luckydice
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,25 +11,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,21 +33,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 import pl.ignismark.luckydice.data.Result
 import pl.ignismark.luckydice.data.ResultRepository
 import pl.ignismark.luckydice.ui.theme.LuckyDiceTheme
-import pl.ignismark.luckydice.ui.theme.Shapes
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
@@ -120,7 +106,7 @@ fun RollHistoryScreen(
             .fillMaxSize()
     ) {
         LazyColumn {
-            items(ResultRepository.mockResults) { result ->
+            items(ResultRepository.results) { result ->
                 RollResultItem(
                     result = result,
                     modifier = Modifier.padding(dimensionResource(id = R.dimen.micro_padding))
